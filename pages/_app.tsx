9dff7b6200fcac8cbb9c25d3a734f223e4afac75
iw17 <iw17@qq.com>
2023-03-07 00:13:34 +0800

    all right

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,4 @@
 import type { AppProps } from "next/app"
-import { createGlobalStyle } from 'styled-components'
-import { ThemeProvider } from 'next-themes'
-
-const GlobalStyle = createGlobalStyle`
-  :root {
-    --bg: transparent;
-    --fg: contrast(--bg);
-  }
-
-  [data-theme="light"] {
-    --bg: white;
-    --fg: black;
-  }
-
-  [data-theme="dark"] {
-    --bg: #111;
-    --fg: #aaa;
-  }
-`
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
