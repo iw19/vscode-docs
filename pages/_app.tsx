708d082682ec55cc25fa2d56b5aa89afbdf742c9
iw17 <iw17@qq.com>
2023-03-07 15:30:43 +0800

    does pages/_app.tsx matter?

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import type { AppProps } from "next/app"
+import { ThemeProvider } from "next-themes"
 import "../styles.css"
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+        <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
