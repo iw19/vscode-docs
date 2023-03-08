b1587a39d6cf1573f12695e43b791471e60c9f2f
iw17 <iw17@qq.com>
2023-03-08 09:23:45 +0800

    tried again with styles/global.css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,5 @@
 import type { AppProps } from "next/app"
-import { useState, useEffect } from "react"
-
-function checkLightDarkStatus(): boolean | undefined {
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches)
-        return true
-    else if (window.matchMedia('(prefers-color-scheme: light)').matches)
-        return false
-    else
-        return undefined
-}
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-//  const [isDark, setIsDark] = useState(true)
-  const isDark = checkLightDarkStatus()
-  return (
-    <>
-      <Component {...pageProps} />
-      <style jsx global>{`${
-        isDark === undefined ? // no-preference, must be transparent
-            "body { background-color: lightgreen; color: red; }" :
-        isDark ? // dark or light
-            "body { background-color: #030; color: greenyellow; }" :
-            "body { background-color: pink; color: yellow; }"
-      }`}</style>
-    </>
-  )
+  return <Component {...pageProps} />
 }
